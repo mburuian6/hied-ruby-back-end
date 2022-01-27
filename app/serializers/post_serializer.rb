8477190471c743525f4488b4087a872a9b20c2ca
Ian Mburu <mburu@finplusgroup.com>
2022-01-27 08:55:14 +0300

    Add resource for collection

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -9,5 +9,15 @@ class PostSerializer
     "/posts/#{resource.id}"
   end
 
+  collection of: 'posts' do
+    attribute(:count) { resources.count }
+    link :self do
+      format "/posts%s", (options[:page] && "?page=#{options[:page]}")
+    end
+    link :next do
+      "/posts?page=#{options[:next]}" if options[:next]
+    end
+  end
+
 end
 
