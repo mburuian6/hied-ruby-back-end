01f773c061e8b2333416a0389c848afbc2fef6fb
Ian Mburu <mburu@finplusgroup.com>
2022-01-27 08:54:55 +0300

    Add links for individual resource

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,13 @@
 HALPresenter.base_href = 'http://localhost:8080/'
 class PostSerializer
   extend HALPresenter
+
+  attribute :title
+
+
+  link :self do
+    "/posts/#{resource.id}"
+  end
+
 end
+
