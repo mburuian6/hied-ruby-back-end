3422676630e1a7d2ac63ea497841e3038709b55f
Ian Mburu <mburu@finplusgroup.com>
2022-01-27 09:22:05 +0300

    Add method to mark post as closed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "posts#index"
+
+  resources :posts do
+    put mark_post_as_closed, on: member
+  end
 end
