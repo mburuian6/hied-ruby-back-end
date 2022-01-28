08d8cabca36dab7329d8340bb4e4d2c6f51a291a
Ian Mburu <mburu@finplusgroup.com>
2022-01-28 16:15:22 +0300

    Decode previously generated ids

--- lib/manage_hash_ids.rb
+++ lib/manage_hash_ids.rb
@@ -4,6 +4,10 @@ module ManageHashIds
       generate_hash(min_hash_length: min_hash_length).encode(str)
     end
 
+    def decode(str, min_hash_length)
+      generate_hash(min_hash_length: min_hash_length).decode(str).first
+    end
+
     def generate_hash(min_hash_length: 10)
       alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
       Hashids.new(Rails.application.secret_key_base, min_hash_length, alphabet)
