b0929481a10ed8aecc30985ed03dc174de44ecca
Ian Mburu <mburu@finplusgroup.com>
2022-01-30 13:20:30 +0300

    Separate hash generation for bids

--- lib/manage_hash_ids.rb
+++ lib/manage_hash_ids.rb
@@ -8,6 +8,14 @@ module ManageHashIds
       generate_hash(min_hash_length: min_hash_length).decode(str).first
     end
 
+    def encode_bid(obj_id, min_hash_length: 15)
+      generate_hash(min_hash_length: min_hash_length).encode(obj_id.to_s)
+    end
+
+    def decode_bid(str, min_hash_length)
+      generate_hash(min_hash_length: min_hash_length).decode(str).first
+    end
+
     def generate_hash(min_hash_length: 10)
       alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
       Hashids.new(Rails.application.secret_key_base, min_hash_length, alphabet)
