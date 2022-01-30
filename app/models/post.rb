5f09db8b3ba4fdcd733ed6f4f51502af0254d947
Ian Mburu <mburu@finplusgroup.com>
2022-01-30 13:22:43 +0300

    Associate accepted_bids with their posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   has_many :bids, dependent: :destroy
   has_many :rejected_bids, dependent: :destroy
+  has_many :accepted_bids, dependent: :destroy
 
   def mark_post_as_closed
     update(closed: true)
