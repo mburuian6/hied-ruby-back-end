e279ab81477d1fb2ace546984ebbbdf9a7d02dde
Ian Mburu <mburu@finplusgroup.com>
2022-01-31 16:36:58 +0300

    Create notification_serializer.rb

---
+++ app/serializers/notification_serializer.rb
@@ -0,0 +1,29 @@
+HALPresenter.base_href = 'http://localhost:8080/'
+class NotificationSerializer
+  extend HALPresenter
+
+  attribute :subject
+  attribute :message
+  attribute :owner
+  attribute :notification_references
+  attribute :data
+  attribute :created_at
+  attribute :updated_at
+
+  link :self do
+    "/notifications/#{resource.id}"
+  end
+
+  collection of: 'notifications' do
+    attribute(:count) { resources.count }
+    link :self do
+      format "/notifications%s", (options[:page] && "?page=#{options[:page]}")
+    end
+    link :next do
+      "/notifications?page=#{options[:next]}" if options[:next]
+    end
+  end
+
+end
+
+
