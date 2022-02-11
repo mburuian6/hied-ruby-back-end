cb62d898dd21f8901198e473bd715398b81a4659
Ian Mburu <mburu@finplusgroup.com>
2022-02-11 06:28:20 +0300

    Configure doorkeeper for authentication

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -6,12 +6,30 @@ Doorkeeper.configure do
   orm :active_record
 
   # This block will be called to check whether the resource owner is authenticated or not.
-  resource_owner_authenticator do
-    raise "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
+  # resource_owner_authenticator do
+    # raise "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
     # Put your resource owner authentication logic here.
     # Example implementation:
     #   User.find_by(id: session[:user_id]) || redirect_to(new_user_session_url)
+    # Suggested fixes:
+    #  current_user || redirect_to(new_user_session_url)
+    #  current_user || warden.authenticate!(scope: :user)
+  # end
+
+  resource_owner_from_credentials do |_routes|
+    User.authenticate(params[:email], params[:password])
   end
+
+  # enable password grant
+  grant_flows %w[password]
+
+  allow_blank_redirect_uri true
+
+  skip_authorization do
+    true
+  end
+
+  use_refresh_token
 
   # If you didn't skip applications controller from Doorkeeper routes in your application routes.rb
   # file then you need to declare this block in order to restrict access to the web interface for
