dbad7927234c44c9486d3665196ae68721ae787d
Ian Mburu <mburu@finplusgroup.com>
2022-02-11 06:28:44 +0300

    Configure user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, format: URI::MailTo::EMAIL_REGEXP, uniqueness: true
+  def self.authenticate(email, password)
+    user = User.find_for_authentication(email: email)
+    user&.valid_password?(password) ? user : nil
+  end
+
+    # has_many :access_tokens,
+    #          class_name: 'Doorkeeper::AccessToken',
+    #          foreign_key: :resource_owner_id,
+    #          dependent: :delete_all # or :destroy if you need callbacks
+
 end
