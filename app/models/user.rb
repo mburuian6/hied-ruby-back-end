b018a83c09febd7ef4938ae76be03c7c44016f58
Ian Mburu <mburu@finplusgroup.com>
2022-03-12 16:40:56 +0300

    Username should be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, format: URI::MailTo::EMAIL_REGEXP, uniqueness: true
+  # validates :username, presence: true, uniqueness: true
+
   def self.authenticate(email, password)
     user = User.find_for_authentication(email: email)
     user&.valid_password?(password) ? user : nil
