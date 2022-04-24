2194890c9ad8e591675ff2d16356eb938010a0e2
Ian Mburu <mburu@finplusgroup.com>
2022-04-24 16:52:34 +0300

    Dont send ids

--- app/sidekiq/broadcast_bid_job.rb
+++ app/sidekiq/broadcast_bid_job.rb
@@ -3,5 +3,6 @@ class BroadcastBidJob
 
   def perform(bid_id, job_hash_id)
     bid = Bid.find_by(id: bid_id)
+    ActionCable.server.broadcast("bids_#{job_hash_id}", bid.attributes.except('id', 'post_id'))
   end
 end
