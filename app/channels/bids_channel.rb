df2e4adbba6c8dcc1f30cbd0867587d67033cbe2
Ian Mburu <mburu@finplusgroup.com>
2022-04-24 16:50:52 +0300

    Comment out unused functionality

--- app/channels/bids_channel.rb
+++ app/channels/bids_channel.rb
@@ -7,11 +7,11 @@ class BidsChannel < ApplicationCable::Channel
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def receive(data)
+  # def receive(data)
     # Get from /cable
     # Do something
     # Broadcast to everyone subscribed
-    bid = Bid.find(data['id'])
-    ActionCable.server.broadcast('bids', bid)
-  end
+    # bid = Bid.find(data['id'])
+    # ActionCable.server.broadcast('bids', bid)
+  # end
 end
