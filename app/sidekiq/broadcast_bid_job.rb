a8f86250427580a0212571f2ffdf13c13cf39787
Ian Mburu <mburu@finplusgroup.com>
2022-04-24 17:44:00 +0300

    Broadcast correct attributes

--- app/sidekiq/broadcast_bid_job.rb
+++ app/sidekiq/broadcast_bid_job.rb
@@ -3,6 +3,7 @@ class BroadcastBidJob
 
   def perform(bid_id, job_hash_id)
     bid = Bid.find_by(id: bid_id)
-    ActionCable.server.broadcast("bids_#{job_hash_id}", bid.attributes.except('id', 'post_id'))
+    attributes = bid.attributes.except('id', 'post_id').merge(post_id: bid.post.hash_id)
+    ActionCable.server.broadcast("bids_#{job_hash_id}", attributes)
   end
 end
