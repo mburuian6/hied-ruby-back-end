20baa2b952713c9d3bf691545cee52d545467d03
Ian Mburu <mburu@finplusgroup.com>
2022-04-24 16:52:24 +0300

    Use best practices

--- app/sidekiq/broadcast_bid_job.rb
+++ app/sidekiq/broadcast_bid_job.rb
@@ -1,7 +1,7 @@
 class BroadcastBidJob
   include Sidekiq::Job
 
-  def perform(bid)
-    BidsChannel.server.broadcast('bids', bid)
+  def perform(bid_id, job_hash_id)
+    bid = Bid.find_by(id: bid_id)
   end
 end
