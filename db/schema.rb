1e9f984be47c4931fc4ccf42fcca8f91ee1d2100
Ian Mburu <mburu@finplusgroup.com>
2022-04-25 16:15:41 +0300

    Use correct column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_22_071823) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_25_131041) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_071823) do
     t.string "username", null: false
     t.text "notification_references", default: [], array: true
     t.jsonb "data", default: {}
-    t.integer "type"
+    t.integer "notification_type"
     t.index ["username"], name: "index_notifications_on_username"
   end
 
