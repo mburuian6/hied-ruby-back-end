19ba015491c339eef361ee1a30e346dfba8d4bea
Ian Mburu <mburu@finplusgroup.com>
2022-04-25 16:06:57 +0300

    Update types

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,5 @@
 class Notification < ApplicationRecord
-  enum type: %i[other bid_accept bid_reject post_accepted_bid]
+  enum type: %i[other bid_accepted bid_rejected post_accepted_bid]
 
   belongs_to :user, primary_key: :username, foreign_key: :username
 end
