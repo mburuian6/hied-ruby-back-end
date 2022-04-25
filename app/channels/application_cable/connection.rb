274d1958c01829b444990794df3408529f89d145
Ian Mburu <mburu@finplusgroup.com>
2022-04-25 16:07:12 +0300

    Mark insecure connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    # TODO: Implement security
+    # identified_by :current_bidder
+    #
+    # def connect
+    #   self.current_bidder = find_verified_user
+    # end
+    #
+    # private
+    #
+    # def find_verified_user
+    #   if (verified_user = current_user)
+    #     verified_user
+    #   else
+    #     reject_unauthorized_connection
+    #   end
+    # end
   end
 end
