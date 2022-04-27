7a2f6cdaa121545155ef1142b8a016179dffec31
Ian Mburu <mburu@finplusgroup.com>
2022-04-27 07:24:23 +0300

    Remove unused code

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,43 +2,6 @@ class NotificationsController < ApplicationController
   before_action :set_notification, only: %i[ show update destroy ]
   load_and_authorize_resource
 
-  # # GET /notifications
-  # def index
-  #   @notifications = Notification.all
-  #
-  #   render json: @notifications
-  # end
-  #
-  # # GET /notifications/1
-  # def show
-  #   render json: @notification
-  # end
-  #
-  # # POST /notifications
-  # def create
-  #   @notification = Notification.new(notification_params)
-  #
-  #   if @notification.save
-  #     render json: @notification, status: :created, location: @notification
-  #   else
-  #     render json: @notification.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # # PATCH/PUT /notifications/1
-  # def update
-  #   if @notification.update(notification_params)
-  #     render json: @notification
-  #   else
-  #     render json: @notification.errors, status: :unprocessable_entity
-  #   end
-  # end
-  #
-  # # DELETE /notifications/1
-  # def destroy
-  #   @notification.destroy
-  # end
-
   def all_notifications
     username = params[:username]
     render json: NotificationSerializer.to_collection(
