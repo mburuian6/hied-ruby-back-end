9959827fe7771162024c4a3201df406aecfb8efa
Ian Mburu <mburu@finplusgroup.com>
2022-05-23 16:38:35 +0300

    Auto-add hash-id

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
+require 'manage_hash_ids'
 class Notification < ApplicationRecord
   enum notification_type: %i[other bid_accepted bid_rejected post_accepted_bid]
 
   belongs_to :user, primary_key: :username, foreign_key: :username
+
+  after_create update_hash_id
+
+  def update_hash_id
+    update(hash_id: ManageHashIds.encode(id))
+  end
+
 end
