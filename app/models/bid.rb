7f2bad513474e671b648f54e4f95219bbc4607f2
Ian Mburu <mburu@finplusgroup.com>
2022-05-24 15:33:02 +0300

    Update validations

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,5 +3,5 @@ class Bid < ApplicationRecord
   belongs_to :user, primary_key: :username, foreign_key: :username
 
   validates :pay, presence: true
-  validates :pay, numericality: { greater_than: 0, message: '%{attribute} must be 0 or more' }
+  validates :pay, numericality: { greater_than_or_equal_to: 0, message: ' must be 0 or more' }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,11 +6,12 @@ class Post < ApplicationRecord
   belongs_to :user, primary_key: :username, foreign_key: :username
   # belongs_to_model: foreign_key, has_many_model: primary_key
 
+  enum location: %i[virtual physical]
   scope :open_posts, -> { where(closed: false) }
   scope :closed_posts, -> { where(closed: true) }
 
   validates :title, :pay, :start, :username, presence: true
-  validates :pay, numericality: { greater_than: 0, message: '%{attribute} must be 0 or more' }
+  validates :pay, numericality: { greater_than_or_equal_to: 0, message: ' must be 0 or more' }
 
   def mark_post_as_closed
     update(closed: true)
