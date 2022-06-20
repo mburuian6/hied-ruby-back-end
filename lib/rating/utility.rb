0ab83371577a63ff6bf107ca1b006823aa6f4d68
Ian Mburu <mburu@finplusgroup.com>
2022-06-20 11:46:18 +0300

    Rating deviation

--- lib/rating/utility.rb
+++ lib/rating/utility.rb
@@ -1,5 +1,15 @@
 module Rating
   module Utility
 
+    def initial_rating_deviation(old_deviation=1)
+      potential_deviation = Math.sqrt(old_deviation + 0.96)
+      [potential_deviation, 2].min
+    end
+
+    def new_rating_deviation(old_deviation)
+      initial_rating_deviation(old_deviation)
+    end
+
+
   end
 end
